91ab458f693908d794c3d89c42aade004fed4f66
spidunno <70819261+spidunno@users.noreply.github.com>
2024-08-26 07:25:47 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import P5JS from 'p5';
 import './style.css';
+import type { p5Module } from './types';
+import P5JS from 'p5';
 
-export function setup(p5: P5JS) {
-	p5.createCanvas(500, 500);
-}
-
-export function draw(p5: P5JS) {
-	p5.fill('red');
-	p5.noStroke();
-	p5.ellipse(0, 0, 100, 100);
-}
+export const project: p5Module = {
+  setup: (p5) => {
+    const canvas = p5.createCanvas(window.innerWidth, window.innerHeight);
+    canvas.position(0, 0);
+  },
+  windowResized: (p5) => {
+    p5.resizeCanvas(window.innerWidth, window.innerHeight);
+  },
+  draw: (p5) => {
+    p5.background('black');
+    p5.fill('white');
+    p5.noStroke();
+	p5.circle(p5.width/2, p5.height/2, 50);
+  }
+};
